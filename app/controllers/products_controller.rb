f7b0bcba2fdb9a58675d23684a44648ee4d4a03c
danielschia <daniel.schia@gmail.com>
2026-05-09 09:37:38 -0300

    fix typo on products_controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -42,7 +42,7 @@ class ProductsController < ApplicationController
   private
 
   def set_restaurant
-    @restaurant = current_user.restaurant.find(params[:restaurant_id])
+    @restaurant = current_user.restaurants.find(params[:restaurant_id])
   end
 
   def set_product
